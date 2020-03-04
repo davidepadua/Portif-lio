9a4fc4dbf6fb403c2e3c8c40300fab02eac51143
davidepadua.dev <60408985+davidepadua@users.noreply.github.com>
2020-03-04 19:23:44 -0300

    Add files via upload

---
+++ todolist/script.js
@@ -0,0 +1,39 @@
+//ideia do Luis
+let i = 0;
+let oList = document.getElementById('lista-tarefas');
+
+
+function raschunhoTarefa() {
+    window.text = document.querySelector('#texto-tarefa').value;
+}
+
+
+function addTarefa() {
+    let novaTarefa = document.createElement('LI');
+    let conteudoTarefa = document.createTextNode(text);
+    novaTarefa.appendChild(conteudoTarefa);
+    document.getElementById('lista-tarefas').appendChild(novaTarefa);
+    novaTarefa.classList.add('itemLista');
+    document.getElementById('texto-tarefa').value = '';
+    novaTarefa.id = i;
+    i++;
+}
+
+function apagaTudo() {
+    let a = document.getElementById('lista-tarefas');
+    while (a.firstChild) {
+        a.removeChild(a.firstChild);
+    }
+
+}
+
+function selecionada() {
+    var pegarId = event.target.id;
+    document.getElementById(pegarId).classList.toggle('deixaCinza');
+
+}
+
+function riscadinha() {
+    var pegarId2 = event.target.id;
+    document.getElementById(pegarId2).classList.toggle('completed');
+}
