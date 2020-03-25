404091ad6b63fc8d244314f223f7126bdab268d3
davidepadua.dev <60408985+davidepadua@users.noreply.github.com>
2020-03-25 19:17:43 -0300

    Score Button

--- SiteQuiz/script.js
+++ SiteQuiz/script.js
@@ -2,24 +2,20 @@ function check() {
     let allInputs = document.getElementsByName("flag");
     let selecionados = [];
     let naoSelecionados =[];
-    let corretos = [];
-    
-    for (let i = 0; i<18 ;i++){
-        if (allInputs[i].checked === true){
+    let corretos = [];    
+    for (let i = 0; i<allInputs.length ;i++){
+        if (allInputs[i].checked == true){
         selecionados.push(allInputs[i]); 
-            for (let x= 0; x<selecionados.length; x++){
-            if (selecionados[x].value === 'Certo' ){
-            corretos.push(selecionados[x]);
-                      
-        }  
-        
-        
+            for (let i= 0; i<selecionados.length; i++){
+            if (selecionados[i].value === 'Certo'){
+            corretos.push(selecionados[i])
+            break                                           
+        }
         else { 
-            naoSelecionados.push(allInputs[i]);
-            console.log(naoSelecionados);
+            naoSelecionados.push(allInputs[i]);            
         }
     }
 }}
-alert('Voce acertou'+'  ' + corretos.length + '  entre 10!');
+alert('Voce acertou'+'  ' + corretos.length + '  entre 9!');
 }
 
