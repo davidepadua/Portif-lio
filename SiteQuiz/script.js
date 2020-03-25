f29a95d9e87f2f67e45e247c01af89eb1ea7db93
davidepadua <davidepadua1914@gmail.com>
2020-03-25 18:13:27 -0300

    funcao botao

--- SiteQuiz/script.js
+++ SiteQuiz/script.js
@@ -3,8 +3,7 @@ function check(){
     let selecionados = [];
     let naoSelecionados =[];
     let corretos = [];
-    let incorretos = [];
-
+    
     for (let i = 0; i<18 ;i++){
         if (allInputs[i].checked === true){
         selecionados.push(allInputs[i]); 
@@ -12,7 +11,7 @@ function check(){
             if (selecionados[x].value === 'Certo' ){
             corretos.push(selecionados[x]);
             alert('Voce acertou'+'  ' + corretos.length + '  entre 10!')
-        }               
+        }  
         
         
         else { 
@@ -21,20 +20,4 @@ function check(){
         }
     }
 }}}
-    
-    
-    /*
-    var results = [];
-    for(var x=0;x<18;x++)
-        if(allInputs[x].value == 'Certo')
-            results.push(allInputs[x]);
-            console.log(results);
-    return results;    
-}
 
-function check() {
-        if (){        
-        console.log('Tudo preenchido');
-    }else {  
-        console.log('Nada preenchido');
-}}*/
