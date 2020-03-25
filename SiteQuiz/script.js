816c9d84dfa62aefc366a2deeca110c580d39e74
davidepadua.dev <60408985+davidepadua@users.noreply.github.com>
2020-03-25 18:23:44 -0300

    fixed alert

--- SiteQuiz/script.js
+++ SiteQuiz/script.js
@@ -1,4 +1,4 @@
-function check(){
+function check() {
     let allInputs = document.getElementsByName("flag");
     let selecionados = [];
     let naoSelecionados =[];
@@ -10,14 +10,16 @@ function check(){
             for (let x= 0; x<selecionados.length; x++){
             if (selecionados[x].value === 'Certo' ){
             corretos.push(selecionados[x]);
-            alert('Voce acertou'+'  ' + corretos.length + '  entre 10!')
+                      
         }  
         
         
         else { 
-            naoSelecionados.push(allInputs[i])
+            naoSelecionados.push(allInputs[i]);
             console.log(naoSelecionados);
         }
     }
-}}}
+}}
+alert('Voce acertou'+'  ' + corretos.length + '  entre 10!');
+}
 
